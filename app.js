f49909b75325cc26a978cf29b5ca23ca3f616365
keshav_regmi <keshavregmi70@gmail.com>
2024-08-27 17:48:47 +0530

    Created Views for frontend

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 // app.js
 const express = require("express");
-
+const path = require("path");
 const app = express();
+
+// Serve static assets (HTML pages)
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "index.html"))
+);
+app.get("/register", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "register.html"))
+);
+app.get("/login", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "login.html"))
+);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
